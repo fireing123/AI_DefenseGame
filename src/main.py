1ebaea36e28a6ab27586d8313f1af7a24fbe946e
fireing123 <88523379+fireing123@users.noreply.github.com>
2023-08-03 15:48:43 +0900

    commit

--- src/main.py
+++ src/main.py
@@ -3,14 +3,17 @@ import color
 
 class AiDefenseGame:
     
-    def __init__(self, size=(500, 500)):
+    def __init__(self, size=(500, 500), tick=60):
         pygame.init()
+        pygame.display.set_caption("AI Defense Game")
+        self.width,self.height = size
         self.screen = pygame.display.set_mode(size=size)
         self.is_running = True
+        self.clock = pygame.time.Clock()
     
-    def create_text(self, object_name=None , str="", size=50, color=color.WHITE, center=(250, 250)):
+    def create_text(self, object_name=None , str="", size=50, color=color.WHITE, center=(400, 300)):
         font = pygame.font.Font(object_name, size=size)
-        text = font.render(text=str, antialias=True, color=color)
+        text = font.render(str, True, color)
         text_rect = text.get_rect()
         text_rect.center = center
         self.screen.blit(text, text_rect)
@@ -28,18 +31,21 @@ class AiDefenseGame:
     def start(self):
         self.screen.fill((0, 0, 0))
         self.create_text(str="Ai Defense Game")
-        self.create_text(str="Press a key to play", size=20, center=(250, 150))
-        pygame.display.flip()
+        self.create_text(str="Press a key to play", size=20, center=((400, 400)))
+        pygame.display.update()
         self.wait_key()
+        
                      
     def main(self):
 
+        self.screen.fill(color.WHITE)
         while self.is_running:
-
+            self.clock.tick(60)
 
             for event in pygame.event.get(): # 
                 if event.type == pygame.QUIT:
-                    is_running = False
+                    self.is_running = False
+            pygame.display.update()
 
         pygame.quit() # 종료
         
@@ -53,5 +59,6 @@ class AiDefenseGame:
 print("ani is real")
 
 if __name__ == "__main__" :
-    game = AiDefenseGame()
+    game = AiDefenseGame(size=(800, 600), tick=60)
     game.start()
+    game.main()
