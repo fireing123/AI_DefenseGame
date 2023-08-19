7339b13413a693ffc70b910f52089559454ae5f9
fireing123 <88523379+fireing123@users.noreply.github.com>
2023-08-19 19:06:40 +0900

    commit

--- src/enemy.py
+++ src/enemy.py
@@ -8,7 +8,7 @@ class Enemy(GameObject):
     def __init__(self, layer=3):
         super().__init__(layer)
 
-class Soldier(GameObject):
+class Soldier(Enemy):
     """
     적군인 오브젝트 전진, 발사기능이 있음
     """
