3a33b854d7dea2f20520a901032b770efe95c417
fireing123 <88523379+fireing123@users.noreply.github.com>
2023-08-20 12:12:35 +0900

    sheeet

--- src/sprites.py
+++ src/sprites.py
@@ -1,14 +1,19 @@
 import pygame
-from typing import List
-
+from typing import Dict
+import xml.etree.ElementTree as xml
 
 class SpriteSheet:
     def __init__(self, xml_path):
-        self.full_image = pygame.image.load("")
-        self.images : List[pygame.Surface] = []
-        for rect in "":
-            self.images += pygame.transform.chop(self.full_image, rect)
-            
+        docs = xml.parse(xml_path)
+        root = docs.getroot()
+        self.full_image = pygame.image.load(root.attrib['imagePath'])
+        self.images : Dict = []
+        for child in root:
+            att = child.attrib
+            left_top = int(att['x']), int(att['y'])
+            width_height =  int(att['width']), int(att['height'])
+            rect = pygame.Rect(left_top, width_height)
+            self.images[att['name']] = pygame.transform.chop(self.full_image, rect)
         
     def __len__(self):
         return self.images.__len__()
@@ -16,4 +21,6 @@ class SpriteSheet:
     def __getitem__(self, index):
         return self.images[index]
     
+    def __setitem__(self, index, value):
+        self.images[index] = value
     
