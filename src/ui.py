07a4aad649819a366333b9b20fdd7452ee7199c0
soyun0418 <141238798+soyun0418@users.noreply.github.com>
2023-08-23 14:47:59 +0900

    1

--- src/ui.py
+++ src/ui.py
@@ -193,23 +193,33 @@ class Text(UI):
         self.rect = self.image.get_rect()
         
 class HPbar(UI):
-    def __init__(self):
-        super.__init__()
-    
-    def show(self,sprite_image,position : list,hp,maxhp,Surface,type): #순서대로 이미지,위치[x,y],hp,maxhp,표면,타입
-        self.x , self.y = position[0], position[1] # x,y좌표 설정
-        self.rect = sprite_image.get_rect(center = (self.x, self.y)) #중심 xy기준으로 사각형얻기
-        self.hp = hp
-        self.maxhp = maxhp
+    def __init__(self, name, position : tuple[int, int]):
+        super.__init__(name)
+        self.rect = self.image.get_rect()
+        self.position = position
+        self.rect.center = position
 
-        if type == 1: #ai를 타입1로
-            color = (0,0,255)
-        elif type == 2: #enemy를 타입2로
-            color = (255,0,0)
+    def render(self, surface):
+        pass
 
 
-        pygame.draw.rect(Surface,(30,30,30),[self.rect.centerx-(self.rect[2]*1.1)/2,self.rect.top-25,self.rect[2]*1.1,10])
-        pygame.draw.rect(Surface,color,[self.rect.centerx-(self.rect[2]*1.1)/2,self.rect.top-25,((self.rect[2]*1.1)/maxhp)*hp,10])
+#    def render(self, surface):
+ #       surface.blit(self.image, self.rect)    
+
+#    def render(self,sprite_image,position : list,hp,maxhp,Surface,type): #순서대로 이미지,위치[x,y],hp,maxhp,표면,타입
+#        self.x , self.y = position[0], position[1] # x,y좌표 설정
+#        self.rect = sprite_image.get_rect(center = (self.x, self.y)) #중심 xy기준으로 사각형얻기
+#        self.hp = hp
+#        self.maxhp = maxhp
+#
+#        if type == 1: #ai를 타입1로
+#            color = (0,0,255)
+#        elif type == 2: #enemy를 타입2로
+#            color = (255,0,0)
+#
+#
+#        pygame.draw.rect(Surface,(30,30,30),[self.rect.centerx-(self.rect[2]*1.1)/2,self.rect.top-25,self.rect[2]*1.1,10])
+#        pygame.draw.rect(Surface,color,[self.rect.centerx-(self.rect[2]*1.1)/2,self.rect.top-25,((self.rect[2]*1.1)/maxhp)*hp,10])
 
 #HPbar코드 입맛대로 뜯어고치고 재탕해둘것
 
