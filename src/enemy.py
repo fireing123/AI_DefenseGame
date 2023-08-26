7fd30817023377c5882e5807e50146231ae5eafc
fireing123 <88523379+fireing123@users.noreply.github.com>
2023-08-26 23:34:05 +0900

    class?

--- src/enemy.py
+++ src/enemy.py
@@ -1,16 +1,27 @@
 
+from pygame import Surface
 from object import GameObject
 
 
 class Enemy(GameObject):
     """
     """
-    def __init__(self, layer=3):
-        super().__init__(layer)
+    def __init__(self, name):
+        super().__init__(name)
 
 class Soldier(Enemy):
     """
     적군인 오브젝트 전진, 발사기능이 있음
     """
+    
+    def __init__(self, name):
+        super().__init__(name)
+        self.health = 100
+    
+    def update(self):
+        pass
+    
+    def render(self, surface: Surface, camera: tuple):
+        pass
     
     
--- src/player.py
+++ src/player.py
@@ -2,7 +2,7 @@ import pygame
 from typing import Dict
 #not import my module
 import manger
-from object import GameObject
+from object import LivingObject
 from camera import camera
 #import module first
 from animation import AnimationController # object
@@ -11,7 +11,7 @@ from ground import group, Ground # object
 
 
 
-class Player(GameObject):
+class Player(LivingObject):
     
     
     
@@ -21,9 +21,9 @@ class Player(GameObject):
         get_width, get_height = manger.screen.get_size()
         self.width, self.height = get_width/2, get_height/2
         self.health = 100
-        self.direction = pygame.Vector2(0, 0)
+
         self.speed = 2
-        self.gravity = 0.08
+
         self.jump_speed = -6
         idle_animation = SpriteSheet('src/image/ai/config.xml')
         self.animation_controller = AnimationController(
@@ -50,24 +50,7 @@ class Player(GameObject):
             self.mass = True
     
     def update(self):   
-        collison = pygame.sprite.spritecollide(self, group, False)
-        
-        self.direction.y += self.gravity
-        
-        if self.mass:
-            self.direction.x *= self.friction
-            if abs(self.direction.x) < 0.1:
-                self.direction.x = 0
-        
-        if collison != None:
-            for collide in collison:
-                self.on_collision_enter(collide)
-
-            self.friction = 0.4
-        else:
-            self.friction = 0.5
-        self.position = self.rect.center
-        self.position += self.direction
+        super().update()
 
         if self.rect_position[0] > self.width * 0.85:
             camera.x += 1
@@ -79,42 +62,11 @@ class Player(GameObject):
        # self.animation_controller.update()
     
     def on_collision_enter(self, collision : Ground):
-        cox, coy = collision.rect.size
-        cox, coy = cox/2, coy/2
-        if self.rect.bottom < collision.rect.top + coy:
-            self.rect.bottom = collision.rect.top + 1
-            self.direction.y = self.mn_numbrt(self.direction.y)
-            self.on_ground = True
-        else:
-            self.direction.y += self.gravity
-            if self.rect.top > collision.rect.bottom - coy:
-                #bottom
-                self.rect.top = collision.rect.bottom
-                self.direction.y = self.process_number(self.direction.y)
-            elif self.rect.left > collision.rect.right - 5:
-                #right
-                self.rect.left = collision.rect.right
-                self.direction.x = 0
-            elif self.rect.right < collision.rect.left + 5:
-                #left
-                self.rect.right = collision.rect.left
-                self.direction.x = 0
+        pass
         
     def jump(self):
         self.direction.y = self.jump_speed
         self.on_ground = False
-    
-    def process_number(self, num):
-        if num < 0:
-            return 0
-        else:
-            return num
-    
-    def mn_numbrt(self, num):
-        if num > 0:
-            return 0
-        else:
-            return num
     
     def render(self, surface, camera):
         cx, cy = camera
